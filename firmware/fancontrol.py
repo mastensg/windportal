707abe4fcefa7070e076892a204084f7fe70e065
Jon Nordby <jononor@gmail.com>
2018-10-09 13:23:30 +0200

    firmware: Update to use PWM on pyMata

--- firmware/fancontrol.py
+++ firmware/fancontrol.py
@@ -1,18 +1,39 @@
 
 import gevent
-from pyfirmata import Arduino, util
+import numpy
+from PyMata.pymata import PyMata
 
+def toggle(board):
+    board.digital_write(2, False)
+    gevent.sleep(2.0)
+    board.digital_write(2, True)
+    gevent.sleep(2.0)
+    board.digital_write(2, False)
+    gevent.sleep(2.0)
+    board.digital_write(2, True)
 
 def main():
     port = '/dev/ttyUSB0'
-    board = Arduino(port)
 
-    print('on')
-    board.digital[13].write(1)
-    gevent.sleep(3.5)
-    print('off')
-    board.digital[13].write(0)
-    gevent.sleep(3.5)
+    board = PyMata(port, verbose=True)
+
+    FAN_PIN = 3
+
+    board.set_pin_mode(13, board.OUTPUT, board.DIGITAL)
+    board.set_pin_mode(FAN_PIN, board.PWM, board.ANALOG)
+
+    d = numpy.sin(numpy.linspace(0, 3.14, 100))
+    print('d', d)
+
+    for _ in range(0, 10):
+        for v in list(d):
+            print(v)
+            board.analog_write(FAN_PIN, (int)(v*255))
+            gevent.sleep(0.01)
+
+    #board.set_pin_mode(PUSH_BUTTON, board.INPUT, board.DIGITAL, cb_push_button)
+    #board.set_pin_mode(POTENTIOMETER, board.INPUT, board.ANALOG, cb_potentiometer)
+
 
 if __name__ == '__main__':
     main()

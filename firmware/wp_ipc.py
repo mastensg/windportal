547c6e309660e28c10acfc9e6df3936faa6359f9
Martin Stensgård <mastensg@mastensg.net>
2018-10-29 23:29:35 +0100

    wp_ipc: support receiving messages

--- firmware/wp_ipc.py
+++ firmware/wp_ipc.py
@@ -64,7 +64,7 @@ def main():
     else:
         print('listening for messages')
         while True:
-            r = session.recv()
+            r = s.recv()
             if {} != r:
                 print(r)
 

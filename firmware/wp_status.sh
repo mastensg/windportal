e9e0b8247374878d50b9833ab332bbcb46be3908
Martin Stensgård <mastensg@mastensg.net>
2018-11-02 16:52:01 +0100

    firmware/wp_status: moden [vp]id, mmcli, control leds

--- firmware/wp_status.sh
+++ firmware/wp_status.sh
@@ -19,16 +19,11 @@ arduino_is_attached() {
 
 # TODO(mastensg): VID & PID for modem
 modem_is_attached() {
-    lsusb -d 0000:0000 >/dev/null
+    lsusb -d 12d1:1506 >/dev/null
 }
 
-# TODO(mastensg):
 modem_link_is_up() {
-    # ip link ?
-    # ip address ?
-    # mmcli ?
-
-    false
+    mmcli -m 0 | grep '  state:' | grep -q "'connected'$"
 }
 
 hub_is_connected() {
@@ -37,6 +32,39 @@ hub_is_connected() {
 
 all_services_are_running() {
     ! systemctl is-failed --quiet '*'
+}
+
+##############################################################################
+
+# LED control
+
+LED_SLEEP_SECONDS="0.1"
+
+led_enable() {
+	local pin="${1}"
+
+	if [ ! -e "/sys/class/gpio/gpio${pin}" ]
+	then
+		echo "${pin}" > /sys/class/gpio/export
+		sleep "${LED_SLEEP_SECONDS}"
+	fi
+
+	echo "out" > "/sys/class/gpio/gpio${pin}/direction"
+	sleep "${LED_SLEEP_SECONDS}"
+}
+
+led_off() {
+	local pin="${1}"
+
+	echo "0" > "/sys/class/gpio/gpio${pin}/value"
+	sleep "${LED_SLEEP_SECONDS}"
+}
+
+led_on() {
+	local pin="${1}"
+
+	echo "1" > "/sys/class/gpio/gpio${pin}/value"
+	sleep "${LED_SLEEP_SECONDS}"
 }
 
 ##############################################################################
@@ -54,14 +82,50 @@ output_led() {
     local status_name="${1}"
     local status="${2}"
 
-    printf "%-25s %s\n" "${status_name}" "${status}"
+    # -     Fan connected
+    # -     Arduino power
+    # -     Raspberry Pi power
+    # G10   Modem attached
+    # G9    Modem link up
+    # G11   Arduino attached
+    # G5    Hub connected
+    # G6    All services running
+
+    case "${status_name}" in
+        ("arduino_is_attached")
+            local led_pin="10"
+            ;;
+        ("modem_is_attached")
+            local led_pin="9"
+            ;;
+        ("modem_link_is_up")
+            local led_pin="11"
+            ;;
+        ("hub_is_connected")
+            local led_pin="5"
+            ;;
+        ("all_services_are_running")
+            local led_pin="6"
+            ;;
+    esac
+
+    led_enable "${led_pin}"
+
+    case "${status}" in
+        ("true")
+            led_on "${led_pin}"
+            ;;
+        ("false")
+            led_off "${led_pin}"
+            ;;
+    esac
 }
 
 output_mqtt() {
     local status_name="${1}"
     local status="${2}"
 
-    printf "%-25s %s\n" "${status_name}" "${status}"
+    true
 }
 
 ##############################################################################
